33b2b67e2520db1a221a59d7e2fa0acbf3b07caa
Sahilthorve09 <149391162+Sahilthorve09@users.noreply.github.com>
2025-02-11 12:11:25 +0530

    Thisis paeky blinders

---
+++ script.js
@@ -0,0 +1,298 @@
+const episodesData = {
+    1: [
+      { 
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABcTmnLi0XFKHu0XLByVqHMzXeWchS7Tj25R04m67DMSiA6gBRydPN4ZOE4qFMZYkT1Bflv3eDKh-0MtstAxYpVxV5fTrSvm0USajpM9xXjtDDeLWTw9n374I.jpg?r=c76',
+        title: 'Episode 1',
+        duration: '57 min',
+        description: 'Ambitious gang leader Thomas Shelby recognizes an opportunity to move up in the world thanks to a missing crate of guns.'
+      },
+  
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABd69ZrAdPV3O_J2ZNH9Cnk2tH6UUfHz9IPfIvfFjvPzBurNtTAfGrlk4MuhmRKWIL50bWKRooQAHmPMdcfhgg6Jb7T05vb8li4LngaaTIL-D9tcpnNuPNges.jpg?r=c06',
+        title: 'Episode 2',
+        duration: '58 min',
+        description: 'Thomas provokes a local kingpin by fixing a horse race and starts a war with a gypsy family; Inspector Campbell carries out a vicious raid.'
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABZUcbxChnZKvn0qb0IbDqgY-gg2zi1i9_Cp4OGlmZaxe7jeA_wQKtHLvlanT23EJFTFwDbr0yfLBaSabYKRfrmme9fL9Ei61-W1KqSKQkYksXeOJJYcg2pMj.jpg?r=b69',
+        title: 'Episode 3',
+        duration: '28 min',
+        description: 'Thomas schemes to get closer to Billy Kimber at the Cheltenham races, and considers an offer from IRA sympathizers to buy his stolen guns'
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABX6eeukNNqd9tzVlTZp5DN3oiS8eTa4nyX9Z_V5Gx2J6CAiOZugr3KmVlJOfnxlrWuhuVuhC9YGfWlvvAAvg99CWeO91RGTR7WKbsjJ421FhUDAfhSQHgb6o.jpg?r=753',
+        title: 'Episode 4',
+        duration: '31 min',
+        description: "As his war with the Lee family escalates, Thomas harbors suspicions about his brother's fiancée; Campbell pressures the gang to deliver the guns."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABQkgYboPOowk0wp8W8j0eU_MBUjv5KSA3XUCzeeCs-iT67FI8YfKWwLQuKiMLhMQQTYZ2Wdqg_ubl4kalGgKunsJlwCNZVEDt9l72AQNnCTbYvPZQkkxd6VV.jpg?r=a07',
+        title: 'Episode 5',
+        duration: '29 min',
+        description: "Thomas deals with an IRA chief bent on avenging his cousin's death; Grace must decide whether her loyalties lie with Campbell or Thomas."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABS6uJKMlBOMzg0hjA22pXTNWkylUhkQ2oNSEX0CWeyECcn1Pzr482NwKAScSCrhPqMnxdWwaa6aDitP6rIIuZ3PxMQkQw6edho9HYM15BGGfDwwZLPxoopoV.jpg?r=e86',
+        title: 'Episode 6',
+        duration: '33 min',
+        description: 'Thomas prepares to oust Billy Kimberfrom power, but family secrets that come to light and the obsessed Campbell may prove his undoing.'
+      }
+    ],
+    2: [
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABZZ7vmM3hJZ-Pjq1f1ud61L54NWN1GSwqQ9RfV9hQMUgyLAQzBuYaGmA3ETYpQY8UTjHi3wRRhTAWHOy6y-s4Nz3DP_SmwNkraspXkC6HPl7_qz-91-zfgwR.jpg?r=f51',
+        title: 'Episode 1',
+        duration: '30 min',
+        description: "When Birmingham crime boss Tommy Shelby's beloved Garrison pub is bombed, he finds himself blackmailed into murdering an Irish dissident."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABVQw_RDfMi68fnEGHCSaRp6q0sg6uyTUJfqiOdsVmo9-1gNtR6mrVG0OkDK4BOjE8EjM4h2Sw5pBmyvhvP8wNDzCsvvz61raNndlp5NO_r8AOzGC29IdWeKn.jpg?r=5a6',
+        title: 'Episode 2',
+        duration: '32 min',
+        description: "After murdering an Irish dissident, Tommy has no choice but to be a pawn in Inspector Campbell's treacherous political game."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABV290jgerq9jeoRP6XvYEycfdkpPfXebfxrdyumXCP6T6-46wbCoNAboHo_ivZoRuZx9-uJ0dRCg6wYrXkoHzBQFN3a4xhQMBXdp7eeOQqcCZj4pQfualEk3.jpg?r=a44',
+        title: 'Episode 3',
+        duration: '28 min',
+        description: "After joining London crime boss Alfie Solomons in a business venture, Tommy worries that Alfie's newly revealed volatility could pose a problem."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABR4Y77KYvZRACW3U0AJQ9kT3NKdsES5d2GfYmF1RhFy6KxLnldUQ4u9wy-mrIHwz5of1G6KqUOEIlafAhMoHvLkWBIyDzTdnLfbUHsXvY7dti9cJz7J3fCZv.jpg?r=fba',
+        title: 'Episode 4',
+        duration: '31 min',
+        description: "The Peaky Blinders take over London's Eden Club; Sabini convinces his old adversary Alfie Solomons to join forces and eradicate the gang."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABc-pMnn4joMZFQvfKaeKcFnjn8OYpE0SzEwtrYX1S-BmVZjvYYfcGVnbLmIS5jN5XBtBPChfx3vsgNZ467V9aNxqhGD2RP_VszMDw52Zvygw314U50NifZRr.jpg?r=63b',
+        title: 'Episode 5',
+        duration: '29 min',
+        description: "The Peaky Blinders are under attack. Tommy's power base in London is obliterated, and both Arthur and Michael are arrested and imprisoned."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABQ5TPtSmyKTF1Y7GN3xI0wsoagVFjnkcR-rVF0ZMYnFfAjwwy5GyO1IxNJVXtOr8ylgrM8e_SyxyD5Hvg_1vFUjf8MrbQIWwZ9xe3FrwEDH0kZ8Gre6BuBok.jpg?r=856',
+        title: 'Episode 6',
+        duration: '33 min',
+        description: " Tommy plans to execute the mission given to him by Campbell: the assassination of a high-ranking member of the military establishment"
+      }
+    ],
+    3: [
+      {
+        img: "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABT-B-mrdN2Um8PS6EgNPr9sbUklfT4xodKpBVyztg5EUBC_IbLvssZfoWVcDoB75vtUcR-J2Uar2j89PpsBkI4IzMSwjGr0Z0sH-DAKkkvDWXcKsOWnZSuTk.jpg?r=9cd",
+        title: 'Episode 1',
+        duration: '30 min',
+        description: "On Tommy's wedding day, a new business partner arranges a secret transaction. Arthur has found a godly woman, and Polly meets an attractive man."
+      },
+      {
+        img: "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABRimC3Ct9CBkzwBREuz13SJdzP6Oy5NDbdI77QUHoi9d36MIt6SnOmLlIpVhd6xKbmQcwi2yRghc2Kl4jQxE2Oh9d45Tx9bf3al1uY8_atEbCDm58o4jRN-f.jpg?r=58a",
+        title: 'Episode 2',
+        duration: '32 min',
+        description: "A careless threat sparks a war. Tommy gets a rare form of payment, but his bosses flex their power to keep him in line. Polly commissions a portrait"
+      },
+      {
+        img: "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABZbJViZa_kPpzWJApN7ikjlYPCw6Q5hG_Pv166OaQ_rpu2FhleN8NNK9nl9I2liAHKdQQKQRtgpMqa0gPxlbh3x3cW-x9ZZCQNvpi_MKeCeZmLrfsdh34w0N.jpg?r=18c",
+        title: 'Episode 3',
+        duration: '28 min',
+        description: "Tommy travels to Wales seeking absolution, and uncovers a traitor in the Economic League. Michael develops a taste for guns. Arthur gets good news."
+      },
+      {
+        img: "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABUMisr59J3mRnh1PF_VFcKI6m5dMJGJ3t8CkC10v7NY87WWxm2ZOt-RcUbdK7LHG16_QvObDDGRjxVWJ3ZtNyxhXubMtXzorvw4qY7yZWYRbuKCO-CyCwpSD.jpg?r=73f",
+        title: 'Episode 4',
+        duration: '31 min',
+        description: "Polly goes to confession, igniting a chain of events that reveals a trap being laid at the Shelbys' expense. Tommy plans an exit from dirty business."
+      },
+      {
+        img: "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABSGM-II__97qjOtXat4lqiv2-Mzy95yk_tbSqMKcEOiRDLg5d-_80KlwYcc4H920KQKeIXDjuv3nms7OHI-H1yvPv22hZxPiA4lysAV3X3U1oN2qdp-du_Ok.jpg?r=9c7",
+        title: 'Episode 5',
+        duration: '29 min',
+        description: "Realizing he's being outmaneuvered by the Russians, Tommy seeks help from a former business partner. Polly's new friendship becomes something more."
+      },
+      {
+        img: "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABXIlIND8FxVNqZc1uP97gGu5iWf7OZBZzq8jqVSsZUVoxLoWJfCkbF-2810G-DlLnCtqb-JJfShfAQkDq4HQm2yYrSMDNsnsUeWbIwx5TeBJ4jjj_jBC_CeG.jpg?r=249",
+        title: 'Episode 6',
+        duration: '33 min',
+        description: "As Tommy prepares to commit the most audacious crime of his career, an unexpected blow forces him to face his worst fears in a race against time."
+      }
+    ],
+    4: [
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABfGxzIHg7s2rmlAZL5LTSvLw_ZGKNHgvZczsFO1SkkVgx7hWTFLbx8jr5sZZJQYZBUFFhnjFPxeYpjuaYy-6VI0A7ZlPXRTRbs67ok6HyKmbfjUc46I8ojhV.jpg?r=173',
+        title: 'Episode 1',
+        duration: '30 min',
+        description: "At Christmastime 1925, members of the fractured Shelby family get word that the New York Mafia is coming to avenge murders they committed a year ago."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABTQwTThqlqSYeTWBm26djPqn1joEL_MSqQbOUzRDKQEXvPTuGiyb2yl_yVrMFdKhKZPOSkd5sO93dBtd-Tp4_Qm1lFqMsU0gD3ktJd021L8zTCO63CYM7J94.jpg?r=add',
+        title: 'Episode 2',
+        duration: '32 min',
+        description: "Reeling from their loss, the Shelbys gather in Small Heath, where Tommy calls for a family truce in order to launch an offensive against the Mafia."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABU6P1UYrOWz3mvO6rd8PlrCq1dv-JLEfsBC-HvIvn8JkoHwW5_bk9WA1cgQJl_HtnOOC6fckCaaGgvTw6sb5P1DqutEqLfz-GcmwCJzhavnlYs0-bIitUVIq.jpg?r=c61',
+        title: 'Episode 3',
+        duration: '28 min',
+        description: "Linda tries to distract Arthur, who later gets caught in an ambush. Tommy is reminded of who he was before the war. Polly meets a man from her past."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABVhm1drVvBk58M9ScYlgxgmw-VxHGN8LN22yTNefbB-cOQb0dllJ5NN7jWCTfruGwqvxLDioYjtym0jZSvMj0FR0miSZmxSrJGhXKjgwFB8HBNf7DhfJyW9j.jpg?r=e3a',
+        title: 'Episode 4',
+        duration: '31 min',
+        description: "The Mafia tries to lure one of the Shelbys into a trap. Tommy is visited by an old flame. Lizzie receives unexpected news."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABXWk8XThbzu4YCfQI37Tdap08RLJGU8hixtUxwhR8b-T-LO13X3nWe16S1jJS39XE3VrrCW-nrGEqdMOPfJjU1em2B6MVXMbCxfE6oygHUyn4zgzkM6R8Qc2.jpg?r=15d',
+        title: 'Episode 5',
+        duration: '29 min',
+        description: "Despite being outnumbered, Tommy starts a shootout with the Mafia. Luca Changretta approaches Alfie Solomons. The British military seeks out Ada."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABZwOfc3fdd4xsdUIuMIpuVHRVV5199cSNPEai45JFkmMaaESF3-C5t8yOImK9ObqhM1iKJvnYQKQGxXV1mXfWoTdxfTqQOjqBhJKPEonfynckzjkdWeWKhQd.jpg?r=cbc',
+        title: 'Episode 6',
+        duration: '33 min',
+        description: "During the boxing match, Arthur follows a hunch. Tommy is inspired by some words of wisdom from Alfie Solomons and gets valuable intel from Jessie."
+      }
+    ],
+    5: [
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABXb4d9U9jrNiB_kgwBcZn1HI9XWi7g1iCYPwSM-UGP5zTGu6NIoxqlVvXx8uby_k44SJ4abtZ4m8NcueJ6F9YbG8H4YYltPwESw5Fv7mtkU0qY5B-ePI2iEV.jpg?r=f09',
+        title: 'Episode 1',
+        duration: '30 min',
+        description: "Grave news from the U.S. sends the Shelbys to an urgent board meeting. Ada hides a secret. Tommy is revisited by his past, in the form of a journalist."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABfAJpbzXFaOap94qqTrx2MqJEQ027KWwdyi1N5W7OcLuowqmUKo7IfUExM9cHhzLlsGK881-_hogl1JcHuX3XjKEtYrlVUXVTeoWTtemg9NFr-rPJc5W2T8d.jpg?r=fd8',
+        title: 'Episode 2',
+        duration: '32 min',
+        description: "The anonymous messages take a terrifying turn. Michael's loyalties are called into question. Tommy receives an offer from Oswald Mosley."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABRMQW-Vloxw4KoryZQWZd59F_BX7uBHqVJWAdkLLdpdI1-fD7tjT85Ij2PxIaoBbbKge2GiHfDyRnY9eT6D3qjiXVvcdjGI-WyDsEKnJxvYCTlumIYcQb0BK.jpg?r=621',
+        title: 'Episode 3',
+        duration: '28 min',
+        description: "Aberama heads to Glasgow to get revenge. Mosley blackmails Tommy, who decides to turn the tables on him. Frantic to find Linda, Arthur loses control."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABRam-HKDrxGe2-ys54XVe2ouJq4Q66T_x7CcNcBJQTeO1HT6VpIg9ToQmnw-nc5WKekGpN8Fi4RjCdJBtQzSKphETvV0LF3qH0HrNVO0oEQiN0E0EyQmLba6.jpg?r=31e',
+        title: 'Episode 4',
+        duration: '31 min',
+        description: "Tommy calls a truce and meets with Jimmy McCavern. A lucrative business opportunity presents itself. Aberama is asked to put love before revenge."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABUWWae51ER70W0r-hhdDgGmI5aQ7E7hJM-vU51p-rCv4QufTikFXWaUpkKCZDUh7f6RiO3UrHkqVBlvELCyL1cCjJVtKbCUINYC3dRh9_gxkcrEvss2AOMPv.jpg?r=7d0',
+        title: 'Episode 5',
+        duration: '29 min',
+        description: "At the party, Mosley makes a speech. Ben Younger faces unexpected consequences for reporting Tommy's intel on fascists. Tommy visits an asylum patient."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABbIcLFyOdUqFDuD8wIqe1CmSAzf0fQnVwZUH904TMAzEEMozTH6Yi-5omkUUzKocYonalYdAYeDH2extmckFQa3VGIJPN6CpH2_RDIgOGHb5OI4O5Pt7seJC.jpg?r=57b',
+        title: 'Episode 6',
+        duration: '33 min',
+        description: "At a family meeting, Michael makes a bold proposal. The mole is identified. Tommy asks an old rival for help with his plan to take down Mosley."
+      }
+    ],
+    6: [
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABanXjbqC8pY5AK44tpa7Mjh7SNIGtAoI-H7kqbZHQHsuB6hC1GziolH_gxrB2hwTntp0qQClGhbsAeKuvz4njhCwqt0TdAoGZmfVGQJBm8TENg8fA-WK9ZQ1.jpg?r=a9c',
+        title: 'Episode 1',
+        duration: '30 min',
+        description: "The foiled assassination of Mosley costs the Shelbys a family member. Four years later, the imminent end of Prohibition brings Tommy to North America."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABaxo4pvGSbaSvPsk60ZDOlC3WhSByAet3EW0G-rwAiYE1S75c6s3wzVy8fwm36OPnnW76umA5nVTm1qsFyDMujqTlH60az5Lz_nXZRDDL1W07FxKIn5AH-Ay.jpg?r=f3b',
+        title: 'Episode 2',
+        duration: '32 min',
+        description: "Back home, Tommy forges an alliance with the unlikeliest of people to sweeten his proposal to Jack Nelson. Daughter Ruby makes a troubling claim."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABfHy_ks7m0ET9HTqhJOgesduA1BpwIyPDbws37tHGkzaQ857X2Vv5gNokxbWLsygr21kXwscxkuNJ8hCklLmsjLgEbnoUAGMl3worKW0agXAyQTds6FmW2k3.jpg?r=69b',
+        title: 'Episode 3',
+        duration: '28 min',
+        description: "Tommy races to find Esme. In his stead, Ada introduces Jack Nelson to Mosley and Diana. Arthur meets a recovered addict who understands his struggle."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABfLGtkiIYS8DqIuWgRXfYThvuIN7_vUe9TJtGD-n3oggfellMPNGZ0y8QW2wf_0TSetY2JR4omiaigMb-1XYtHFhcbAzmu28oinzhxNlolPHZ6Nlhy7bWBtl.jpg?r=422',
+        title: 'Episode 4',
+        duration: '31 min',
+        description: "Devastated by another loss, Tommy sets out to get revenge. Fascism hits close to home for Ada. An urgent message arrives from the tuberculosis clinic."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABXJTKCvF992y2KDFMtcVbuj4AieL22Ex-q0DioxNBR9txfOxvrT7OANI8_SfH8XMacMCTom3BHB33wIBItQAg7OrF8fIysoi1dxsCjrPIs1Mpde4Ay88CmU-.jpg?r=4b4',
+        title: 'Episode 5',
+        duration: '29 min',
+        description: "Tommy makes a deal with Linda. The mole lurking among the Shelbys is given a new mission. Diana agrees to do a favor for Tommy — on one condition."
+      },
+      {
+        img: 'https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABaGvLeoG4zp-CDDdX8uUY6Xo-mJMjPgDaHAbmtHSaY-O45c_dcUaqbjoBKleYjcGLkkLOv4_-SzEyo57ALIGM0NwZ_y4FiuTkw8.jpg?r=bcc',
+        title: 'Episode 6',
+        duration: '33 min',
+        description: "The mole learns when and where Arthur will be alone. Fresh out of prison, Michael sets out on his mission. Finn's loyalty to the Shelbys is tested."
+      }
+    ]
+  };
+  
+  function updateEpisodes() {
+    const season = document.getElementById('season').value;
+    const episodesContainer = document.getElementById('episodes');
+    episodesContainer.innerHTML = '';
+  
+    episodesData[season].forEach(ep => {
+      const episodeElement = document.createElement('div');
+      episodeElement.classList.add('episode');
+      episodeElement.innerHTML = `
+            <img src="${ep.img}" alt="Episode Image">
+            <h3>${ep.title}</h3>
+            <h5>Duration: ${ep.duration}</h5>
+            <p>${ep.description}</p>
+        `;
+      episodesContainer.appendChild(episodeElement);
+    });
+  }
+  
+// video content 
+
+const slider = document.querySelector('.slider');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const playPauseButtons = document.querySelectorAll('.play-pause');
+const videos = document.querySelectorAll('.slide video');
+
+let index = 0;
+const totalSlides = videos.length - 2; // Show 3 at a time
+
+// Update slider position
+function updateSlider() {
+    slider.style.transform = `translateX(-${index * (100 / 3)}%)`;
+}
+
+// Next button functionality
+next.addEventListener('click', () => {
+    if (index < totalSlides - 1) {
+        index++;
+        updateSlider();
+    }
+});
+
+// Previous button functionality (fixed)
+prev.addEventListener('click', () => {
+    if (index > 0) {
+        index--;
+        updateSlider();
+    }
+});
+
+// Play/Pause Button Functionality
+playPauseButtons.forEach((button, i) => {
+    button.addEventListener('click', () => {
+        if (videos[i].paused) {
+            videos[i].play();
+            button.textContent = '❚❚'; // Pause icon
+        } else {
+            videos[i].pause();
+            button.textContent = '►'; // Play icon
+        }
+    });
+
+    videos[i].addEventListener('ended', () => {
+        button.textContent = '►';
+    });
+});
